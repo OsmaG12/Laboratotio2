8cb9ad91b8d3bac8f0269e4a7094162c8fb5be5e
oscar <oscar.martz511@gmail.com>
2024-04-05 23:32:50 -0600

    Add project files.

---
+++ Laboratotio2/Controllers/VistasRazorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratotio2.Controllers
+{
+    public class VistasRazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
